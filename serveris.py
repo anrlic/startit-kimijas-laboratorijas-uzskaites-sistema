2909e4ee5a7510efa8e54ac8a8e06968cf2fefc7
Andris Līcītis <andris.licitis@gmail.com>
2020-12-29 09:20:43 +0000

    izveidots route sveiki

--- serveris.py
+++ serveris.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 def index():
     return "Labrīt!"
 
+@app.route('/sveiki')
+def sveiki():
+    return "Nav vairs nekāds rīts!"
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
+
+    # debug=True automatiski parstartee 
+    # serveri pec izmainaam .py failaa
