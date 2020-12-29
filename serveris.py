620b41ff3205c7f42dae896a85a15879a4ff6121
Andris Līcītis <andris.licitis@gmail.com>
2020-12-29 09:48:10 +0000

    rezinasa rote

--- serveris.py
+++ serveris.py
@@ -12,6 +12,20 @@ def index():
 def sveiki():
     return "Nav vairs nekāds rīts!"
 
+# dinamiskie celi8
+@app.route('/sveiki/<vards>')
+def sveikiPersona(vards):
+    # 1 varioants
+    #return "Sveiki {}".format(vards)
+    # 2 variants
+    return f"Sveiki {vards}"
+
+@app.route('/cik/<sk1>/<sk2>')
+def rezinajums(sk1, sk2):
+  sk1=int(sk1)
+  sk2=int(sk2)
+  r=sk1*sk2;
+  return str(r)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
